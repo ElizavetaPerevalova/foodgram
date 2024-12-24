60a7468b2d921341507cc671d336644b101c8b6f
Елизавета Перевалова <lisaperevalova1@gmail.com>
2024-12-25 01:39:18 +0500

    hm

--- backend/api/management/commands/load_ingredients.py
+++ backend/api/management/commands/load_ingredients.py
@@ -1,4 +1,5 @@
 import csv
+import logging
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -7,25 +8,52 @@ from api.models import Ingredient
 
 
 class Command(BaseCommand):
+    help = 'Импорт данных из csv-файла в базу данных (модель Ингредиенты)'
 
-    PATH_DATA, FILE_NAME = 'data', 'ingredients.csv'
+    def add_arguments(self, parser):
+        parser.add_argument('--path', type=str, help='Path to file')
 
-    PATH = settings.BASE_DIR.parent.joinpath(PATH_DATA)
+    def handle(self, *args, **options):
+        success_count = 0
+        with open(
+                f'{settings.BASE_DIR}/data/ingredients.csv',
+                'r',
+                encoding='utf-8',
+        ) as csv_file:
+            reader = csv.reader(csv_file)
 
-    FIELD_NAMES = ('name', 'measurement_unit')
+            for row in reader:
+                name_csv = 0
+                unit_csv = 1
+                try:
+                    obj, created = Ingredient.objects.get_or_create(
+                        name=row[name_csv],
+                        measurement_unit=row[unit_csv],
+                    )
+                    if created:
+                        success_count += 1
+                except Exception as error:
+                    logging.exception(f'Ошибка в строке {row}: {error}')
+# class Command(BaseCommand):
 
-    help = f'''Populates Database with the Data from csv-File Located within
-{PATH_DATA}'''
+#     PATH_DATA, FILE_NAME = 'data', 'ingredients.csv'
 
-    def handle(self, *args, **options) -> None:
+#     PATH = settings.BASE_DIR.parent.joinpath(PATH_DATA)
 
-        with open(self.PATH.joinpath(self.FILE_NAME), encoding='utf8') as file:
-            reader = csv.DictReader(file, fieldnames=self.FIELD_NAMES)
-            Ingredient.objects.bulk_create(Ingredient(**_) for _ in reader)
+#     FIELD_NAMES = ('name', 'measurement_unit')
 
-        self.stdout.write(
-            self.style.SUCCESS(
-                f'''Successfully Populated Database with the Data from
-csv-File Located within {self.PATH_DATA}'''
-            )
-        )
+#     help = f'''Populates Database with the Data from csv-File Located within
+# {PATH_DATA}'''
+
+#     def handle(self, *args, **options) -> None:
+
+#         with open(self.PATH.joinpath(self.FILE_NAME), encoding='utf8') as file:
+#             reader = csv.DictReader(file, fieldnames=self.FIELD_NAMES)
+#             Ingredient.objects.bulk_create(Ingredient(**_) for _ in reader)
+
+#         self.stdout.write(
+#             self.style.SUCCESS(
+#                 f'''Successfully Populated Database with the Data from
+# csv-File Located within {self.PATH_DATA}'''
+#             )
+#         )
