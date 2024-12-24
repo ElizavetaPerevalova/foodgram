e710b77bebe93efcb52f8930c889bad99703c9c0
Елизавета Перевалова <lisaperevalova1@gmail.com>
2024-12-25 01:42:33 +0500

    om

--- backend/api/management/commands/load_ingredients.py
+++ backend/api/management/commands/load_ingredients.py
@@ -34,26 +34,3 @@ class Command(BaseCommand):
                         success_count += 1
                 except Exception as error:
                     logging.exception(f'Ошибка в строке {row}: {error}')
-# class Command(BaseCommand):
-
-#     PATH_DATA, FILE_NAME = 'data', 'ingredients.csv'
-
-#     PATH = settings.BASE_DIR.parent.joinpath(PATH_DATA)
-
-#     FIELD_NAMES = ('name', 'measurement_unit')
-
-#     help = f'''Populates Database with the Data from csv-File Located within
-# {PATH_DATA}'''
-
-#     def handle(self, *args, **options) -> None:
-
-#         with open(self.PATH.joinpath(self.FILE_NAME), encoding='utf8') as file:
-#             reader = csv.DictReader(file, fieldnames=self.FIELD_NAMES)
-#             Ingredient.objects.bulk_create(Ingredient(**_) for _ in reader)
-
-#         self.stdout.write(
-#             self.style.SUCCESS(
-#                 f'''Successfully Populated Database with the Data from
-# csv-File Located within {self.PATH_DATA}'''
-#             )
-#         )
